4a063685c4818abb5b11cc3ffb923ca9d81a9ee2
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2022-07-26 19:54:28 -0500

    Second changes

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+import { division, multiplication, subtract, sum } from "lib/functions";
+// TEST
+console.log(division(30, 5));
+console.log(multiplication(12, 12));
+console.log(subtract(650, 550));
+console.log(sum(450, 650));
