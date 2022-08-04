db12adec9976e823a1c63f97492957f352390682
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2022-08-04 13:14:49 -0500

    fourth and last change

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import { division, multiplication, subtract, sum } from "lib/functions";
-// TEST
-console.log(division(30, 5));
-console.log(multiplication(12, 12));
-console.log(subtract(650, 550));
-console.log(sum(450, 650));
--- index.js
+++ index.js
@@ -1,39 +1,55 @@
-"use strict";
-exports.__esModule = true;
-var functions_1 = require("./lib/functions");
-var buttonOne = document.getElementById('one');
-buttonOne.addEventListener("click", function () { return screenNumber(buttonOne.value); });
-var buttonTwo = document.getElementById('two');
-buttonTwo.addEventListener("click", function () { return screenNumber(buttonTwo.value); });
-var buttonThree = document.getElementById('three');
-buttonThree.addEventListener("click", function () { return screenNumber(buttonThree.value); });
-var buttonFour = document.getElementById('four');
-buttonFour.addEventListener("click", function () { return screenNumber(buttonFour.value); });
-var buttonFive = document.getElementById('five');
-buttonFive.addEventListener("click", function () { return screenNumber(buttonFive.value); });
-var buttonSix = document.getElementById('six');
-buttonSix.addEventListener("click", function () { return screenNumber(buttonSix.value); });
-var buttonSeven = document.getElementById('seven');
-buttonSeven.addEventListener("click", function () { return screenNumber(buttonSeven.value); });
-var buttonEight = document.getElementById('eight');
-buttonEight.addEventListener("click", function () { return screenNumber(buttonEight.value); });
-var buttonNine = document.getElementById('nine');
-buttonNine.addEventListener("click", function () { return screenNumber(buttonNine.value); });
-var divisionButton = document.getElementById('division');
-divisionButton.addEventListener("click", functions_1.division);
-var multiplicationButton = document.getElementById('multiplication');
-multiplicationButton.addEventListener("click", functions_1.multiplication);
-var subtractButton = document.getElementById('subtract');
-subtractButton.addEventListener("click", functions_1.subtract);
-var sumButton = document.getElementById('sum');
-sumButton.addEventListener("click", functions_1.sum);
-var result = document.getElementById('result');
-result.addEventListener("click", functions_1.operate);
-function screenNumber(val) {
-    result.innerHTML = val;
+import { division, multiplication, operate, subtract, sum } from "./lib/functions.js"
+
+const buttons = [
+	document.getElementById('one'),
+	document.getElementById('two'),
+	document.getElementById('three'),
+	document.getElementById('four'),
+	document.getElementById('five'),
+	document.getElementById('six'),
+	document.getElementById('seven'),
+	document.getElementById('eight'),
+	document.getElementById('nine'),
+	document.getElementById('zero'),
+	document.getElementById('decimal-point'),
+	document.getElementById('division'),
+	document.getElementById('multiplication'),
+	document.getElementById('subtract'),
+	document.getElementById('sum'),
+]
+
+let result = document.getElementById('result')
+
+for (const button of buttons) {
+	button?.addEventListener("click", () => {
+		result.value = result.value + button.value;
+	});
 }
-// TEST
-console.log((0, functions_1.division)(30, 5));
-console.log((0, functions_1.multiplication)(12, 12));
-console.log((0, functions_1.subtract)(650, 550));
-console.log((0, functions_1.sum)(450, 650));
+
+const equalButton = document.getElementById("equal")
+
+equalButton?.addEventListener("click", () => {
+	const operator = getOperator(result.value);
+	const [numOne, numTwo] = result.value.split(operator);
+
+	let results = operate(operator, numOne, numTwo);
+	result.value = results;
+});
+
+function getOperator(string) {
+	const operators = ['+', '-', 'x', '/'];
+	const stringToArr = string.split('');
+
+	for (const char of stringToArr) {
+		if (operators.includes(char)) {
+			return char;
+		}
+	}
+}
+
+let resetButton = document.getElementById('reset')
+resetButton.addEventListener('click', resetGame)
+
+function resetGame() {
+	location.reload()
+}
